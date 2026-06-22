64b347b03ffa7d1d12bcd558bb70d89438b8114d
ahmedalarapy1 <ahmedalarapy2002@gmail.com>
2026-06-23 01:41:24 +0300

    solve Elziro Assignment

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let person="Mr:- Mohamed Sa'id"
+let testimonialAdd=`
+<div class="testimonial--add">
+                    <img src="images/icons/facebook.png" alt="person">
+                    <h6>Mr:- ${person}</h6>
+                    <h4>RN , churge nurse </h4>
+                    <p>Ahmed is one of the pest Egyption nurses ever he has a larg scale of knowledge</p>
+                    `;
+document.write(testimonialAdd);
+let d ="-100";
+let e ="20";
+let f = 30 ; 
+let g= true;
+console.log(-d * +e)
+console.log(-d +e*2 + f +g*3)
